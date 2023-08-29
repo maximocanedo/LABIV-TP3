a9d4d1426b4452ed2d4184fa643c55fdbe3cdc7b
Máximo Canedo <103539894+maximocanedo@users.noreply.github.com>
2023-08-29 11:56:57 -0300

    Crear excepción DNIInvalidoException

---
+++ src/ejercicio1/DNIInvalidoException.java
@@ -0,0 +1,14 @@
+package ejercicio1;
+
+public class DNIInvalidoException extends Exception {
+
+	public DNIInvalidoException() {
+		
+	}
+
+	@Override
+	public String getMessage() {
+		return "El DNI ingresado no es válido. ";
+	}
+	
+}
